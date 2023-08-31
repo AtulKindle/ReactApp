4ca731926f03cae64a912fe21de6389023373263
Atul <atul.kumar@kindlebit.net>
2023-08-31 19:08:59 +0530

    Gallery component

---
+++ src/components/Gallery.js
@@ -0,0 +1,9 @@
+export default function Gallery(){
+    return(
+        <div class="text-center">
+            <h1 class="text-primary">
+                Hello React App 
+            </h1>
+        </div>
+    )
+}
