a0657866fac1f9006bdaaa7707fbf77e311bbb14
Atul <atul.kumar@kindlebit.net>
2023-09-04 17:42:48 +0530

    login design

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,21 +1,37 @@
+
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { 
+        day: 'numeric',
+        month: 'long',
+        weekday: 'short',
+    }
+  ).format(date);
+}
 export default function Login(){
     const Avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
     return(
         <div className="login-wrapper mt-5 p-5 shadow-lg">
             <h2><img src= {Avatar} alt="" className="rounded-pill img-fluid"/> Create Your Account</h2>
             <form class="row g-3">
-                <div class="col-12">
+                <div className="col-12">
                     <label for="exampleFormControlInput1" class="form-label ">Email address</label>
-                    <input type="password" class="form-control mb-3" id="inputPassword2" placeholder="Password"/>
+                    <input type="password" className="form-control mb-3" id="inputPassword2" placeholder="Password"/>
                 </div>
-                <div class="col-12">
+                <div className="col-12">
                     <label for="exampleFormControlInput1" class="form-label mb-3">Email address</label>
-                    <input type="password" class="form-control mb-3" id="inputPassword2" placeholder="Password"/>
+                    <input type="password" className="form-control mb-3" id="inputPassword2" placeholder="Password"/>
                 </div>
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary mb-3">Submit</button>
+                <div className="col-12">
+                    <button type="submit" className="btn btn-primary mb-3">Submit</button>
                 </div>
             </form>
+            <div className="text-end">
+                <p class="fw-light">{formatDate(today)}</p>
+            </div>
         </div>
     );
 }
