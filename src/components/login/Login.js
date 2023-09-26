d3a7940150570599a314131b6427d6f9c9c99746
Atul <atul.kumar@kindlebit.net>
2023-09-26 12:06:00 +0530

    react router installation

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,37 +1,67 @@
-
 const today = new Date();
 
 function formatDate(date) {
-  return new Intl.DateTimeFormat(
-    'en-US',
-    { 
-        day: 'numeric',
-        month: 'long',
-        weekday: 'short',
-    }
-  ).format(date);
+  return new Intl.DateTimeFormat("en-US", {
+    day: "numeric",
+    month: "long",
+    weekday: "short",
+  }).format(date);
 }
-export default function Login(){
-    const Avatar = 'https://loremflickr.com/320/240/dog';
-    return(
-        <div className="login-wrapper mt-5 p-5 shadow-lg">
-            <h2><img src= {Avatar} alt="" width={64} className="rounded-pill img-fluid"/> Create Your Account</h2>
-            <form class="row g-3">
-                <div className="col-12">
-                    <label for="exampleFormControlInput1" class="form-label ">Email address</label>
-                    <input type="password" className="form-control mb-3" id="inputPassword2" placeholder="Password"/>
-                </div>
-                <div className="col-12">
-                    <label for="exampleFormControlInput1" class="form-label mb-3">Email address</label>
-                    <input type="password" className="form-control mb-3" id="inputPassword2" placeholder="Password"/>
-                </div>
-                <div className="col-12">
-                    <button type="submit" className="btn btn-primary mb-3">Submit</button>
-                </div>
-            </form>
-            <div className="text-end">
-                <p class="fw-light">{formatDate(today)}</p>
+export default function Login() {
+  const Avatar = "https://loremflickr.com/320/240/dog";
+  return (
+        <div className="login-wrapper mt-5 p-0 p-md-5 shadow-lg w-100 w-md-50 mx-auto">
+     <div className="row">
+        <div className="col-lg-12">
+    <div className="container">
+      <h2>
+        <img
+          src={Avatar}
+          alt=""
+          width={64}
+          className="rounded-pill img-fluid"
+        />{" "}
+        Create Your Account
+      </h2>
+      <form>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="mb-3">
+              <label for="exampleFormControlInput1" class="form-label ">
+                Email address
+              </label>
+              <input
+                type="password"
+                className="form-control mb-3"
+                id="inputPassword2"
+                placeholder="Password"
+              />
             </div>
+            <div className="mb-3">
+              <label for="exampleFormControlInput1" class="form-label mb-3">
+                Email address
+              </label>
+              <input
+                type="password"
+                className="form-control mb-3"
+                id="inputPassword2"
+                placeholder="Password"
+              />
+            </div>
+            <div className="mb-3">
+              <button type="submit" className="btn btn-primary mb-3">
+                Submit
+              </button>
+            </div>
+          </div>
         </div>
-    );
+      </form>
+      <div className="text-end">
+        <p class="fw-light">{formatDate(today)}</p>
+      </div>
+      </div>
+        </div>
+     </div>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
+import './components/login/Login';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+import Login from './components/login/Login';
+import Home from './pages/Home';
+// import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 
 
-
-const Client = new ApolloClient({
-  uri: "https://rickandmortyapi.com/graphql",
-  cache: new InMemoryCache(),
-})
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ApolloProvider client = {Client}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>
-);
+export default function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>}> </Route>
+          <Route path="*" element={<Home/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App />);
+// const Client = new ApolloClient({
+//   uri: "https://rickandmortyapi.com/graphql",
+//   // uri: "https://jsonplaceholder.typicode.com/users",
+//   cache: new InMemoryCache(),
+// })
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <ApolloProvider client = {Client}>
+//       <App />
+//     </ApolloProvider>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
